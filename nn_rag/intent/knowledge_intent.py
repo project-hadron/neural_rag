36ba22cf450785ceaff6c4e5ceb2b956d92d20cb
gigas64 <gigas64@opengrass.net>
2024-07-09 14:05:05 -0500

    - add tqdm bars in knowledge_intent.py

--- nn_rag/intent/knowledge_intent.py
+++ nn_rag/intent/knowledge_intent.py
@@ -2,7 +2,7 @@ import ast
 import inspect
 import re
 from collections import Counter
-from tqdm.autonotebook import tqdm
+from tqdm import tqdm
 import pandas as pd
 import pyarrow as pa
 import pyarrow.compute as pc
